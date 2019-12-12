44298e2b5407cff8c0d8c4e8ef9124019c851d86
Gabriel Almeida <gabrielalmeidads@gmail.com>
2019-12-12 13:14:43 -0300

    melhoria index

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <div class="row">
               <div class="col-lg-6">
                 <h1 class="text-white">Gabriel Almeida</h1>
-                <p class="text-secondary">Desenvolvedor<span class="mx-3">|</span>Auxiliar de T.I</p>
+                <p class="text-secondary">Estudante<span class="mx-3">|</span>Desenvolvedor</p>
                 <p class="lead">Me chamo Gabriel estou começando neste mundo enorme chamando de Tecnologia da Informãção,
                  me apresento melhor na seção específica para tal assunto</p>
                 <p><a href="#about-section" class="btn smoothscroll btn-primary">Mais Sobre</a></p>
